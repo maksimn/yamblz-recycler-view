8dd7a34b388b1b1f27fd177b49e1874f6556cafe
maksimn <maksim1989sovunion@yandex.ru>
2018-08-05 20:09:36 +0300

    NumColumnsAnimator.animateChange() some code

--- app/src/main/java/ru/yandex/yamblz/ui/animations/NumColumnsAnimator.java
+++ app/src/main/java/ru/yandex/yamblz/ui/animations/NumColumnsAnimator.java
@@ -1,13 +1,37 @@
 package ru.yandex.yamblz.ui.animations;
 
+import android.animation.ValueAnimator;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class NumColumnsAnimator extends DefaultItemAnimator {
+
     @Override
     public boolean animateChange(RecyclerView.ViewHolder oldHolder,
                                  RecyclerView.ViewHolder newHolder, int fromX, int fromY, int toX,
                                  int toY) {
-        return false;
+        final int durationMs = 650;
+        final View itemView = oldHolder.itemView;
+
+        final int width = itemView.getWidth();
+        final int newWidth = width / 2;
+
+        final ValueAnimator animator = ValueAnimator.ofInt(width, newWidth)
+                .setDuration(durationMs);
+
+        animator.addUpdateListener(animation -> {
+            final int intermediateWidth = (int) animation.getAnimatedValue();
+
+            ViewGroup.LayoutParams layoutParams = itemView.getLayoutParams();
+            layoutParams.width = intermediateWidth;
+
+            itemView.setLayoutParams(layoutParams);
+        });
+
+        animator.start();
+
+        return true;
     }
 }
