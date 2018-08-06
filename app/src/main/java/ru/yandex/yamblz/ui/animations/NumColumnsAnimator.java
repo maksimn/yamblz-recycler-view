d4817134d948c9aae9fc70763865dcaf2e3e4da9
maksimn <maksim1989sovunion@yandex.ru>
2018-08-06 14:37:16 +0300

    animateChange() RecyclerView item fade to white animation

--- app/src/main/java/ru/yandex/yamblz/ui/animations/NumColumnsAnimator.java
+++ app/src/main/java/ru/yandex/yamblz/ui/animations/NumColumnsAnimator.java
@@ -1,13 +1,17 @@
 package ru.yandex.yamblz.ui.animations;
 
-import android.animation.ValueAnimator;
+import android.animation.AnimatorSet;
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
-import android.view.View;
-import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class NumColumnsAnimator extends RecyclerView.ItemAnimator {
+    ArgbEvaluator mColorEvaluator = new ArgbEvaluator();
 
     @Override
     public boolean animateDisappearance(@NonNull RecyclerView.ViewHolder viewHolder, @NonNull ItemHolderInfo preLayoutInfo, @Nullable ItemHolderInfo postLayoutInfo) {
@@ -25,26 +29,25 @@ public class NumColumnsAnimator extends RecyclerView.ItemAnimator {
     }
 
     @Override
-    public boolean animateChange(@NonNull RecyclerView.ViewHolder oldHolder, @NonNull RecyclerView.ViewHolder newHolder, @NonNull ItemHolderInfo preLayoutInfo, @NonNull ItemHolderInfo postLayoutInfo) {
-        final int ANIMATION_DURATION = 1000;
-        final View itemView = oldHolder.itemView;
+    public boolean animateChange(@NonNull RecyclerView.ViewHolder oldHolder,
+                                 @NonNull RecyclerView.ViewHolder newHolder,
+                                 @NonNull ItemHolderInfo preLayoutInfo,
+                                 @NonNull ItemHolderInfo postLayoutInfo) {
+        final int DURATION = 900;
+        TextView itemView = (TextView) oldHolder.itemView;
 
-        final int width = itemView.getWidth();
-        final int newWidth = width / 2;
+        ObjectAnimator bgrToWhite = ObjectAnimator.ofInt(itemView, "backgroundColor",
+                ((ColorDrawable) itemView.getBackground()).getColor(), Color.WHITE);
+        bgrToWhite.setEvaluator(mColorEvaluator);
 
-        final ValueAnimator animator = ValueAnimator.ofInt(width, newWidth)
-                .setDuration(ANIMATION_DURATION);
+        ObjectAnimator textColorToWhite = ObjectAnimator.ofInt(itemView, "textColor", Color.DKGRAY,
+                Color.WHITE);
+        textColorToWhite.setEvaluator(mColorEvaluator);
 
-        animator.addUpdateListener(animation -> {
-            final int intermediateWidth = (int) animation.getAnimatedValue();
-
-            ViewGroup.LayoutParams layoutParams = itemView.getLayoutParams();
-            layoutParams.width = intermediateWidth;
-
-            itemView.setLayoutParams(layoutParams);
-        });
-
-        animator.start();
+        AnimatorSet set = new AnimatorSet();
+        set.playTogether(bgrToWhite, textColorToWhite);
+        set.setDuration(DURATION);
+        set.start();
 
         return true;
     }
